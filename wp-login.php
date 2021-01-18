8213726519fbb771ef8125d9f5090f4f7eb410b5
sainiok <76881248+sainiok@users.noreply.github.com>
2021-01-18 13:27:24 +0200

    Add files via upload

--- wp-login.php
+++ wp-login.php
@@ -4,22 +4,27 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; costassainis cv &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='https://sainiok.github.io/costassainis/wp-includes/css/dashicons.min.css?ver=5.6' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='https://sainiok.github.io/costassainis/wp-includes/css/buttons.min.css?ver=5.6' media='all' />
-<link rel='stylesheet' id='forms-css'  href='https://sainiok.github.io/costassainis/wp-admin/css/forms.min.css?ver=5.6' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='https://sainiok.github.io/costassainis/wp-admin/css/l10n.min.css?ver=5.6' media='all' />
-<link rel='stylesheet' id='login-css'  href='https://sainiok.github.io/costassainis/wp-admin/css/login.min.css?ver=5.6' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://sainiok.github.io/costassainis/wp-includes/css/dashicons.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://sainiok.github.io/costassainis/wp-includes/css/buttons.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://sainiok.github.io/costassainis/wp-admin/css/forms.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://sainiok.github.io/costassainis/wp-admin/css/l10n.min.css?ver=5.6' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://sainiok.github.io/costassainis/wp-admin/css/login.min.css?ver=5.6' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
-		</head>
+	<link rel="icon" href="https://sainiok.github.io/costassainis/wp-content/uploads/2021/01/cropped-imageedit_1_2309255562-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://sainiok.github.io/costassainis/wp-content/uploads/2021/01/cropped-imageedit_1_2309255562-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://sainiok.github.io/costassainis/wp-content/uploads/2021/01/cropped-imageedit_1_2309255562-180x180.png" />
+<meta name="msapplication-TileImage" content="https://sainiok.github.io/costassainis/wp-content/uploads/2021/01/cropped-imageedit_1_2309255562-270x270.png" />
+	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
 	<script type="text/javascript">
 		document.body.className = document.body.className.replace('no-js','js');
 	</script>
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
-	
+	<div class="message">Under Construction Mode is <b>enabled</b>.</div>
+
 		<form name="loginform" id="loginform" action="https://sainiok.github.io/costassainis/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
@@ -53,41 +58,47 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog"><a href="https://sainiok.github.io/costassainis/">
 		&larr; Go to costassainis cv		</a></p>
 			</div>
-	<script src='https://sainiok.github.io/costassainis/wp-includes/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
-<script src='https://sainiok.github.io/costassainis/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
-<script id='zxcvbn-async-js-extra'>
+	<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-includes/js/jquery/jquery.min.js?ver=3.5.1' id='jquery-core-js'></script>
+<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2' id='jquery-migrate-js'></script>
+<script type='text/javascript' id='zxcvbn-async-js-extra'>
+/* <![CDATA[ */
 var _zxcvbnSettings = {"src":"https:\/\/sainiok.github.io\/costassainis\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
 </script>
-<script src='https://sainiok.github.io/costassainis/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
-<script src='https://sainiok.github.io/costassainis/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
-<script id='wp-polyfill-js-after'>
+<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
+<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
+<script type='text/javascript' id='wp-polyfill-js-after'>
 ( 'fetch' in window ) || document.write( '<script src="https://sainiok.github.io/costassainis/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://sainiok.github.io/costassainis/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://sainiok.github.io/costassainis/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://sainiok.github.io/costassainis/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"></scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://sainiok.github.io/costassainis/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"></scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://sainiok.github.io/costassainis/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"></scr' + 'ipt>' );
 </script>
-<script src='https://sainiok.github.io/costassainis/wp-includes/js/dist/i18n.min.js?ver=326fe7fbfdb407b6edbcfba7e17f3909' id='wp-i18n-js'></script>
-<script id='password-strength-meter-js-extra'>
+<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-includes/js/dist/i18n.min.js?ver=326fe7fbfdb407b6edbcfba7e17f3909' id='wp-i18n-js'></script>
+<script type='text/javascript' id='password-strength-meter-js-extra'>
+/* <![CDATA[ */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
 </script>
-<script id='password-strength-meter-js-translations'>
+<script type='text/javascript' id='password-strength-meter-js-translations'>
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script src='https://sainiok.github.io/costassainis/wp-admin/js/password-strength-meter.min.js?ver=5.6' id='password-strength-meter-js'></script>
-<script src='https://sainiok.github.io/costassainis/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
-<script id='wp-util-js-extra'>
+<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-admin/js/password-strength-meter.min.js?ver=5.6' id='password-strength-meter-js'></script>
+<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
+<script type='text/javascript' id='wp-util-js-extra'>
+/* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
 </script>
-<script src='https://sainiok.github.io/costassainis/wp-includes/js/wp-util.min.js?ver=5.6' id='wp-util-js'></script>
-<script id='user-profile-js-translations'>
+<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-includes/js/wp-util.min.js?ver=5.6' id='wp-util-js'></script>
+<script type='text/javascript' id='user-profile-js-translations'>
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script src='https://sainiok.github.io/costassainis/wp-admin/js/user-profile.min.js?ver=5.6' id='user-profile-js'></script>
+<script type='text/javascript' src='https://sainiok.github.io/costassainis/wp-admin/js/user-profile.min.js?ver=5.6' id='user-profile-js'></script>
 	<div class="clear"></div>
 	</body>
 	</html>
